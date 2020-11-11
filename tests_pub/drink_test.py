dcf6193cb771940ce533ca03adb05d861ababa5e
saerx <saerlaith.robyn@gmail.com>
2020-11-11 14:37:39 +0000

    added alcohol level to drink and tested

--- tests_pub/drink_test.py
+++ tests_pub/drink_test.py
@@ -4,7 +4,7 @@ from src_code_pub.drink import Drink
 
 class TestDrink(unittest.TestCase):
     def setUp(self):
-        self.drink = Drink("Guiness", 3)
+        self.drink = Drink("Guiness", 3, 2)
 
     
     def test_drink_has_name(self):
@@ -12,4 +12,7 @@ class TestDrink(unittest.TestCase):
 
     def test_drink_has_price(self):
         self.assertEqual(3, self.drink.price)
+
+    def test_drink_has_alcohol_level(self):
+        self.assertEqual(2, self.drink.alcohol_level)
   
--- tests_pub/pub_test.py
+++ tests_pub/pub_test.py
@@ -15,14 +15,14 @@ class TestPub(unittest.TestCase):
 
     def test_pub_can_sell_drink__over_18(self):
         customer_1 = Customer("Al McWhiggin", 40, 21)
-        drink_1 = Drink("Guiness", 3)
+        drink_1 = Drink("Guiness", 3, 2)
         self.pub.sell_drink(customer_1, drink_1)
         self.assertEqual(37, customer_1.wallet)
         self.assertEqual(203, self.pub.till)
         
     def test_pub_can_sell_drink__under_18(self):
         customer_2 = Customer("Andy", 500, 14)
-        drink_1 = Drink("Guiness", 3)
+        drink_1 = Drink("Guiness", 3, 2)
         self.pub.sell_drink(customer_2, drink_1)
         self.assertEqual(500, customer_2.wallet)
         self.assertEqual(200, self.pub.till)
