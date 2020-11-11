f88b285df27fa6cb8cdc7667babcc2349f1d3e54
saerx <saerlaith.robyn@gmail.com>
2020-11-11 14:20:02 +0000

    added pub_can_sell_drink and tested

--- src_code_pub/pub.py
+++ src_code_pub/pub.py
@@ -5,4 +5,9 @@ class Pub:
         self.till = till
         # self.drinks = drinks
 
-    
+    def sell_drink(self, customer, drink):
+        # Money leaves customer wallet
+        # Increase money in pub till by drink price
+        self.till += drink.price
+        customer.wallet -= drink.price
+
--- tests_pub/pub_test.py
+++ tests_pub/pub_test.py
@@ -1,5 +1,7 @@
 import unittest 
 from src_code_pub.pub import Pub 
+from src_code_pub.customer import Customer
+from src_code_pub.drink import Drink
 
 class TestPub(unittest.TestCase):
     def setUp(self):
@@ -10,4 +12,11 @@ class TestPub(unittest.TestCase):
     
     def test_pub_has_till(self):
         self.assertEqual(200, self.pub.till)
+
+    def test_pub_can_sell_drink(self):
+        customer_1 = Customer("Al McWhiggin", 40)
+        drink_1 = Drink("Guiness", 3)
+        self.pub.sell_drink(customer_1, drink_1)
+        self.assertEqual(37, customer_1.wallet)
+        self.assertEqual(203, self.pub.till)
 
